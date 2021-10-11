51d2f37dac33c382e6674490ee9610d146d82f2b
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-10-10 22:40:59 -0500

    commented out home.js and homd from app.js and then it start runing from npm run develop

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
+
 import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-import Home from './pages/Home';
-
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
+// import Home from './pages/Home';
 
 const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
+  uri: '/graphql'
 });
 
 function App() {
@@ -21,7 +18,7 @@ function App() {
       <div className="flex-column justify-flex-start min-100-vh">
         <Header />
         <div className="container">
-          <Home />
+          
         </div>
         <Footer />
       </div>
